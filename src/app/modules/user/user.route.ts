3d2cfdaf07d2c2f810944ee548c5eddc7a216f9b
Foysal Ahmed <121689537+foysalahmedmin@users.noreply.github.com>
2025-05-14 17:30:04 +0600

    complete user routes

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,5 +1,31 @@
 import express from 'express';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
+import * as UserControllers from './user.controller';
+import * as UserValidations from './user.validation';
 
 const router = express.Router();
 
-export const UserRoutes = router;
+router.get('/', auth('admin', 'user', 'customer'), UserControllers.getUsers);
+
+router.get('/:id', auth('admin', 'user', 'customer'), UserControllers.getUser);
+
+router.patch(
+  '/:id',
+  auth('admin'),
+  validateRequest(UserValidations.userSchema.partial()),
+  UserControllers.updateUser,
+);
+
+router.delete('/:id', auth('admin'), UserControllers.deleteUser);
+
+router.get('/self', auth('admin', 'user', 'customer'), UserControllers.getSelf);
+
+router.patch(
+  '/self',
+  auth('admin', 'user', 'customer'),
+  validateRequest(UserValidations.userSchema.partial()),
+  UserControllers.updateSelf,
+);
+
+export default router;
